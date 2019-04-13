37d22fa5952f3b8e3638fd79aa4c06ef81afcde8
matsta25 <matim2112@gmail.com>
2019-04-13 20:55:59 +0200

    add workinf dashboard with one input with debouce

--- client-side/src/main.js
+++ client-side/src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// BootstrapVue
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ client-side/src/views/Dashboard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="dashboard">
+    <b-container>
+        <b-row>
+          <b-col cols="4">
+            1. Paste your youtube music URL:
+            </b-col>
+          <b-col cols="8">
+            <b-form-input v-model="youtubeUrl" placeholder="Enter your name"></b-form-input>
+            <small class="text-muted">e.g. https://www.youtube.com/watch?v=smqhSl0u_sI</small>
+          </b-col>
+        </b-row>
+
+    </b-container>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+const debounceTime = 1000 
+
+export default {
+  name: 'dashboard',
+  data () {
+    return {
+      youtubeUrl: ''
+    }
+  },
+  watch: {
+    youtubeUrl: _.debounce((val) => {
+      console.log('Val: ' + val)
+    }, debounceTime)
+  }
+}
+</script>
