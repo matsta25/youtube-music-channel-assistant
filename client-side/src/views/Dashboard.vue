a8f580fb1bf02b4e46a821ab4abce0546f0d57bd
matsta25 <matim2112@gmail.com>
2019-04-14 00:55:38 +0200

    fix eslint errors

--- client-side/src/views/Dashboard.vue
+++ client-side/src/views/Dashboard.vue
@@ -6,7 +6,7 @@
             1. Paste your youtube music URL:
             </b-col>
           <b-col cols="6">
-            <b-form-input v-model="youtubeUrl" placeholder="Enter your name"></b-form-input>
+            <b-form-input v-model="youtubeUrl" v-on:change="onChange" placeholder="Enter your name"></b-form-input>
             <small class="text-muted">e.g. https://www.youtube.com/watch?v=smqhSl0u_sI</small>
           </b-col>
           <b-col cols="2">
@@ -25,53 +25,45 @@
 </template>
 
 <script>
-import _ from 'lodash'
 import Mp3Service from '@/services/Mp3Service'
-import io from 'socket.io-client';
-
-const debounceTime = 1000
+import io from 'socket.io-client'
 
 export default {
   name: 'dashboard',
   data () {
     return {
-      youtubeUrl: 'https://www.youtube.com/watch?v=34ZWp9aMcLs',
-      // youtubeUrl: 'https://www.youtube.com/watch?v=smqhSl0u_sI',
-      socket : io('localhost:8081'),
+      youtubeUrl: '',
+      socket: io('localhost:8081'),
       badgeStatus: {
-        variantType: "light",
+        variantType: 'light',
         text: 'Waiting...',
         err: ''
       }
     }
   },
-  watch: {
-    youtubeUrl: _.debounce(async (url) => {
-      // todo:
-      // this.changeBadge("primary","Downloading.","")
-      const response = await Mp3Service.sendUrl({ url: url })
-    }, debounceTime)
-  },
-  mounted() {
+  mounted () {
     this.socket.on('downloadMp3', (data) => {
-      let response = data.data;
-      if(response.code == 0){
-        this.changeBadge("success","Mp3 downloaded.","")
-      }else{
-        this.changeBadge("danger","Downloading failed.", response.stderr)
+      let response = data.data
+      if (response.code === 0) {
+        this.changeBadge('success', 'Mp3 downloaded.', '')
+      } else {
+        this.changeBadge('danger', 'Downloading failed.', response.stderr)
       }
     })
   },
   methods: {
-    changeBadge(variant, text, err){
+    changeBadge (variant, text, err) {
       this.badgeStatus.variantType = variant
       this.badgeStatus.text = text
-      if(err !== 'undefined'){
-        let n = err.lastIndexOf("ERROR");
+      if (err !== 'undefined') {
+        let n = err.lastIndexOf('ERROR')
         let str = err.substr(n, err.length)
         this.badgeStatus.err = str
       }
-
+    },
+    onChange: async function (url) {
+      this.changeBadge('primary', 'Downloading...', '')
+      await Mp3Service.sendUrl({ url: url })
     }
   }
 }
