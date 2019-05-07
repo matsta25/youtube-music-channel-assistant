0c2c2c0e4dfbbcc38c972ba99e3184433e8a36f7
Mateusz Stańczak <mateusz_stanczak@pgf.com.pl>
2019-05-07 12:28:29 +0200

    Add working .env

---
+++ ymca/server.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- ymca/src/router.js
+++ ymca/src/router.js
@@ -6,7 +6,7 @@ import Signin from "./views/Signin.vue";
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   routes: [
     {
       path: "/",
