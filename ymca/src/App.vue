951557c47d43d4253540fc2d7dc51a0db31645fa
matsta25 <matim2112@gmail.com>
2019-05-12 17:12:32 +0200

    add styled navbar

--- ymca/src/App.vue
+++ ymca/src/App.vue
@@ -1,17 +1,48 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/create">Create</router-link> |
-      <span v-if="this.$store.state.user.displayName">
-        <a href="/logout" @click="logout">Log out ( {{ this.$store.state.user.displayName }}  <b-img :src="this.$store.state.user.photoUrl" width="50" height="50" rounded="circle"></b-img> )</a>
-      </span>
-      <span v-else>
-        <router-link to="/signin">Sign in</router-link>
-      </span>
-    </div>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+      <b-container>
+        <b-navbar-brand>
+          <router-link to="/">
+            <img src="https://placekitten.com/g/30/30" class="d-inline-block align-top" alt="Kitten">
+            ymca
+          </router-link>
+        </b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+
+            <b-nav-item>
+              <router-link to="/">Home</router-link>
+              </b-nav-item>
+            <b-nav-item>
+              <router-link to="/create">Create</router-link>
+            </b-nav-item>
+            <b-nav-item>
+              <router-link to="/about">About</router-link>
+            </b-nav-item>
+            <span v-if="this.$store.state.user.displayName">
+              <a href="/logout" @click="logout">Log out ( {{ this.$store.state.user.displayName }}  
+                <b-img :src="this.$store.state.user.photoUrl" width="40" height="40" rounded="circle"></b-img> )
+              </a>
+            </span>
+            <span v-else>
+              <b-nav-item>
+                <router-link to="/signin">Sign in</router-link>
+              </b-nav-item>
+            </span>
+          </b-navbar-nav>
+        </b-collapse>
+    </b-container>
+
+    </b-navbar>
+    <b-container>
     <router-view />
+    </b-container>
   </div>
 </template>
 
