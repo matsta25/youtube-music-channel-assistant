965f74ffc74e47f35673dcf2635ff7e8bfbfc966
matsta25 <matim2112@gmail.com>
2019-05-14 00:05:10 +0200

    clean server-side & client-side & extract ymca

--- client-side/src/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    // url: 'http://192.168.43.248:8081'
-    url: 'http://localhost:8081'
-}
-
-module.exports = config
--- client-side/src/services/Mp3Service.js
+++
@@ -1,7 +0,0 @@
-import Api from '@/services/Api'
-
-export default {
-  sendUrl (url) {
-    return Api().post('/mp3', { data: url })
-  }
-}
